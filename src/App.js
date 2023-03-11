471b05e2e35790d2990b46229b9932cf3d629a39
„Michal <s22288@pjwstk.edu.pl>
2023-03-11 21:42:31 +0100

    staretd with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asc1',
+          photo: 'https://robohash.org/1?set=set2&size=180x180'
+        },
+        {
+          name: 'Dracula',
+          id: 'asc2',
+          photo: 'https://robohash.org/2?set=set2&size=180x180'
+        },
+        {
+          name: 'Zombie',
+          id: 'asc3',
+          photo: 'https://robohash.org/3?set=set2&size=180x180'
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {
+          this.state.monsters.map((monster) => {
+            return  <h1 key={monster.id}>{monster.name}</h1>
+            
+          })
+          }
+         
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
+
 
 export default App;
